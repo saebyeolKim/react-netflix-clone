2700ac7ede0c6b97f8787902fa673e19bd65b37c
saebyeolKim <toquf2611@naver.com>
2025-09-05 18:46:23 +0900

    영화 나열을 위한 Row 컴포넌트 생성하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import requests from './api/requests';
 import './App.css';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
+import Row from './components/Row';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Nav />
       <Banner />
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTreding}/>
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies}/>
     </div>
   );
 }
--- src/api/requests.js
+++ src/api/requests.js
@@ -2,7 +2,7 @@ const requests = {
     fetchNowPlaying: "/movie/now_playing",
     fetchNetflixOriginals: "/discover/tv?with_networks-213",
     fetchTreding: "/trending/all/week",
-    fechTopRated: "/movie/top_rated",
+    fetchTopRated: "/movie/top_rated",
     fetchActionMovies: "/discover/movie?with_genres=28",
     fetchComedyMovies: "/discover/movie?with_genres=35",
     fetchHorrorMovies: "/discover/movie?with_genres=27",
