d0dfcf523855469493ee48fbcd09cec3f188df29
saebyeolKim <toquf2611@naver.com>
2025-09-06 22:33:47 +0900

    영화 자세히 보기 클릭 시 모달 생성하기

---
+++ src/components/MovieModal/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './MovieModal.css'
+
+function MovieModal({
+    backdrop_path,
+    title,
+    overview,
+    name,
+    release_date,
+    first_air_date,
+    vote_average,
+    setModalOpen
+}) {
+  return (
+    <div>index</div>
+  )
+}
+
+export default MovieModal;
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../api/axios';
 import "./Row.css";
+import MovieModal from './MovieModal';
 
 export default function Row({ isLargeRow, title, id, fetchUrl}) {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState(false);
 
   useEffect(() => {
     fetchMovieData();  
@@ -12,6 +15,11 @@ export default function Row({ isLargeRow, title, id, fetchUrl}) {
   const fetchMovieData = async () => {
     const request = await axios.get(fetchUrl);
     setMovies(request.data.results)
+  }
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
   }
 
   return <section className='row'>
@@ -33,6 +41,7 @@ export default function Row({ isLargeRow, title, id, fetchUrl}) {
           className={`row__poster ${isLargeRow && "row__posterLarge"}`}
           src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
           alt={movie.name}
+          onClick={() => handleClick(movie)}
         />
       ))}
     </div>
@@ -44,6 +53,15 @@ export default function Row({ isLargeRow, title, id, fetchUrl}) {
       }>{">"}
       </span>
     </div>
-    </div>
-  </section>
+  </div>
+
+      {
+        modalOpen && (
+          <MovieModal 
+            {...movieSelected}
+            setModalOpen={setModalOpen}
+          />
+        )
+      }
+</section>
 }
