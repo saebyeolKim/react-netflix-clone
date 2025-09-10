00fae244ec4ef1a6d403f93fce45cf0b945acdf7
saebyeolKim <toquf2611@naver.com>
2025-09-10 14:49:59 +0900

    Netflix 앱에 React Router Dom 적용하기

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import requests from './api/requests';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Banner from './components/Banner';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
-import Row from './components/Row';
+import MainPage from './pages/MainPage';
+import SearchPage from './pages/SearchPage';
+import DetailPage from './pages/DetailPage';
+
+const Layout = () => {
+  return(
+    <div>
+      <Nav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="app">
-      <Nav />
-      <Banner />
-
-      <Row
-        title="NETFLIX ORIGINALS"
-        id="NO"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-
-      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTreding}/>
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated}/>
-      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies}/>
-      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies}/>
-      <Footer />
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<MainPage />}></Route>
+          <Route path=":movieId" element={<DetailPage />}></Route>
+          <Route path='search' element={<SearchPage />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
