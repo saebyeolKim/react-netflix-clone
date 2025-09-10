e0fb7d6abb9acc246db5c0854ba2a02fa2daf53d
saebyeolKim <toquf2611@naver.com>
2025-09-10 15:21:21 +0900

    검색 페이지 UI 구현하기

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,7 @@
 import axios from '../../api/axios';
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import './SearchPage.css'
 
 export default function SearchPage() {
 
@@ -16,7 +17,7 @@ export default function SearchPage() {
 
     useEffect(() => {
         if (searchTerm) {
-            fetchSearchMovie();
+            fetchSearchMovie(searchTerm);
         }
     }, [searchTerm]); // searchTerm 이 변경될 때 마다 해당 useEffect 가 실해되게 하기 위해 넣음
 
@@ -31,7 +32,40 @@ export default function SearchPage() {
         }
     }
 
-    return (
-        <div>index</div>
-    )
+    const renderSearchResults = () => {
+        return searchResults.length > 0 ? (
+            <section className='search-container'>
+                {searchResults.map((movie) => {
+                    if (movie.backdrop_path != null && movie.media_type != 'person') {
+                        const movieImageUrl =
+                        "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
+                        return (
+                            <div className='movie'>
+                                <div
+                                    className='movie__column-poster'
+                                >
+                                    <img
+                                        src={movieImageUrl}
+                                        alt='movie'
+                                        className='movie__poster'
+                                    />
+
+                                </div>
+                            </div>
+                        )
+                    }
+                })}
+            </section>
+        ) : (
+            <section className='no-results'>
+                <div className='no-results__text'>
+                    <p>
+                        찾고자하는 검색어 "{searchTerm}"에 맞는 영화가 없습니다.
+                    </p>
+                </div>
+            </section>
+        )
+    }
+
+    return renderSearchResults();
 }
